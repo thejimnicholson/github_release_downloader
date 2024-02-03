f32f88facd34c400fffc5cfa5dad6b250382e43a
Jim Nicholson <thejimnicholson@gmail.com>
2024-02-02 23:29:50 -0800

    Allow user to set the URL for the api server

--- github-release-download.py
+++ github-release-download.py
@@ -6,7 +6,7 @@ import yaml
 import requests
 import argparse
 
-def github_release_download(yaml_file_path=None, download_dir=None):
+def github_release_download(yaml_file_path=None, download_dir=None, base_url='https://api.github.com'):
   # If no YAML file path is provided, use the default file
   if yaml_file_path is None:
       yaml_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'github-releases.yaml')
@@ -22,29 +22,36 @@ def github_release_download(yaml_file_path=None, download_dir=None):
   # Iterate over each item in the YAML file
   for item in data:
     print(f"Looking at {item['name']}")
+
     # Get the latest release
-    response = requests.get(f"https://api.github.com/repos/{item['repository']}/releases/latest")
+    response = requests.get(f"{base_url}/repos/{item['repository']}/releases/latest")
     response.raise_for_status()
     json = response.json()
+
     # Print the name of the release
     print(f"Found release: {json['name']}")
+
     # Process the "files" attribute of the current item
     for file_pattern in item['files']:
+
       # If the file pattern contains special regex characters, compile it as a regex
       if any(char in file_pattern for char in {'.', '^', '$', '*', '+', '?', '{', '}', '[', ']', '\\', '|', '(', ')'}):
         file_regex = re.compile(file_pattern)
         match_func = file_regex.match
       else:
         match_func = file_pattern.__eq__
+
       # Find the asset in the release
       for asset in json['assets']:
         if match_func(asset['name']):
           local_file_path = os.path.join(download_dir, asset['name'])
+
           # If the local file exists and its size is the same as the file to be downloaded, skip the download
           if os.path.exists(local_file_path) and os.path.getsize(local_file_path) == asset['size']:
             print(f"File {asset['name']} already exists and is the same size. Skipping download.")
           else:
             print(f"Downloading {asset['name']}")
+
             # Download the file
             response = requests.get(asset['browser_download_url'], stream=True)
             response.raise_for_status()
@@ -58,7 +65,8 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Download GitHub release assets specified in a YAML file.')
   parser.add_argument('-c', '--config', help='Path to the YAML file.', default=None)
   parser.add_argument('-d', '--dir', help='Directory to download the files to.', default=None)
+  parser.add_argument('-u', '--url', help='Base URL of the GitHub API server.', default='https://api.github.com')
 
   args = parser.parse_args()
 
-  github_release_download(args.config, args.dir)
+  github_release_download(args.config, args.dir, args.url)
